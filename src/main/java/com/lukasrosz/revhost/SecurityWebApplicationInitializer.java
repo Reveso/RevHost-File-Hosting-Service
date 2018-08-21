7ec9ce2b0d7e1ba3e667aa7d947bd9bb842c4af6
Reveso <lukas.rosz@gmail.com>
2018-08-21 22:10:59 +0200

    Project on the move

--- src/main/java/com/lukasrosz/revhost/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.lukasrosz.revhost;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
