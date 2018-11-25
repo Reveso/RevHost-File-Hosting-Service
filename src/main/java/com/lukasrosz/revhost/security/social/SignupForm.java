463791392dd167fe9efd65b04e151c15535a13f7
Reveso <lukas.rosz@gmail.com>
2018-11-25 20:31:37 +0100

    Field errors in signup form

--- src/main/java/com/lukasrosz/revhost/security/social/SignupForm.java
+++ src/main/java/com/lukasrosz/revhost/security/social/SignupForm.java
@@ -5,13 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class SignupForm {
 
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String password;
 
 }
